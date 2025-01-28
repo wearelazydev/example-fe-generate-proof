bf06ece6d79c3ad6be33df4908bb7098a35d7c37
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2025-01-28 21:11:15 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ const CLIENT_ID = "Iv23liCkGlCmiYmxhyZ6";
 
 function App() {
   const [proof, setProof] = useState({});
+  const [pullRequestUrl, setPullRequestUrl] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
   const [rerender, setRerender] = useState(false);
 
   const loginWIthGithub = () => {
@@ -14,20 +16,28 @@ function App() {
   };
 
   const claimReward = async () => {
-    const urlPullRequest = "https://github.com/wearelazydev/repo-tester/pull/1";
+    setIsFetching(true);
 
-    await fetch("https://backend-wearelazydev.up.railway.app/generate-proof?url=" + urlPullRequest, {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("accessToken"),
-      },
-    })
+    // const urlPullRequest = "https://github.com/wearelazydev/repo-tester/pull/1";
+
+    await fetch(
+      "https://backend-wearelazydev.up.railway.app/generate-proof?url=" +
+        pullRequestUrl,
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
+        },
+      }
+    )
       .then((response) => {
         return response.json();
       })
       .then((data) => {
+        console.log(data);
         setProof(data);
-        setRerender(!rerender);
+        setIsFetching(false);
+        setPullRequestUrl("");
       });
   };
 
@@ -39,9 +49,13 @@ function App() {
 
     if (codeParam && localStorage.getItem("accessToken") === null) {
       async function getAccessToken() {
-        await fetch("https://backend-wearelazydev.up.railway.app/getAccessToken?code=" + codeParam, {
-          method: "GET",
-        })
+        await fetch(
+          "https://backend-wearelazydev.up.railway.app/getAccessToken?code=" +
+            codeParam,
+          {
+            method: "GET",
+          }
+        )
           .then((response) => {
             return response.json();
           })
@@ -49,7 +63,7 @@ function App() {
             console.log(data);
             if (data.access_token) {
               localStorage.setItem("accessToken", data.access_token);
-              // setRerender(!rerender);
+              setRerender(!rerender);
             }
           });
       }
@@ -57,24 +71,50 @@ function App() {
     }
   }, []);
 
+  console.log(pullRequestUrl);
+
   return (
     <>
       <h1>Verify Proof</h1>
       {localStorage.getItem("accessToken") ? (
         <>
-          <button onClick={claimReward}>Generate Proof</button>
-          <button
-            style={{ marginLeft: "10px" }}
-            onClick={() => {
-              localStorage.removeItem("accessToken");
-              setRerender(!rerender);
+          <div>
+            <button disabled={isFetching} onClick={claimReward}>
+              Generate Proof
+            </button>
+            <button
+              style={{ marginLeft: "10px" }}
+              onClick={() => {
+                localStorage.removeItem("accessToken");
+                setRerender(!rerender);
+              }}
+            >
+              Logout
+            </button>
+          </div>
+          <div style={{ marginTop: "20px" }}>
+            <input
+              type="text"
+              style={{ width: "100%", padding: "10px 5px" }}
+              placeholder="Provide pull request url here..."
+              onChange={(e) => setPullRequestUrl(e.target.value)}
+              value={pullRequestUrl}
+            />
+          </div>
+          {/* <pre>{proof && <h2>{proof}</h2>}</pre> */}
+          <pre
+            style={{
+              textAlign: "left",
+              padding: "20px",
+              backgroundColor: "#000000",
+              color: "green",
+              borderRadius: "10px",
+              overflow: "auto",
             }}
           >
-            Logout
-          </button>
-          {/* <pre>{proof && <h2>{proof}</h2>}</pre> */}
-          <pre style={{ textAlign: "left", padding: "20px", backgroundColor: "#000000", color: "green", borderRadius: "10px", overflow: "auto" }}>
-            {proof && JSON.stringify(proof, null, 2)}
+            {proof.prProofData
+              ? JSON.stringify(proof, null, 2)
+              : "Waiting for proof..."}
           </pre>
         </>
       ) : (
